167d0605bd851a39677914c9abd8f8ae93e7d99e
momococomimi <kaneokada44@gmail.com>
2026-07-11 22:29:56 +0900

    Update My Best Friend to v5.0.0

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'my-best-friend-v4.4.0';
+const CACHE_NAME = 'my-best-friend-v5.0.0';
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=4.4.0',
-  './script.js?v=4.4.0',
-  './manifest.json?v=4.4.0'
+  './style.css?v=5.0.0',
+  './script.js?v=5.0.0',
+  './manifest.json?v=5.0.0'
 ];
 
 self.addEventListener('install', event => {
